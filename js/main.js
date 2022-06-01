14c021114baa98434d944cd1bb48d0c5c34242d2
Pavel <gerasimov86@gmail.com>
2022-06-01 14:07:58 +0300

    main.js to index / random number function / string length function

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Pick random number
+const numbersArray = [];
+
+function randomNumber(from, to) {
+  if (from < 0 || to < 0) {
+    return ('Number can be only >= 0');
+  } else if (to < from) {
+    return ('Number TO cant be lower than FROM');
+  }
+  for (let i = from; i <= to; i++) {
+    numbersArray.push(i);
+  }
+  const Number = numbersArray[Math.floor(Math.random() * numbersArray.length)];
+  return (`Random number: ${Number}`);
+}
+
+// eslint-disable-next-line no-console
+console.log(randomNumber(37, 1000));
+
+
+//Check string max length
+function stringLength(string, maxlength) {
+  if (string.length > maxlength) {
+    return (`Too long text: ${string.length}, maximum characters: ${maxlength}`);
+  }
+  return (`Text length: ${string.length}, characters left: ${maxlength - string.length}`);
+}
+
+// eslint-disable-next-line no-console
+console.log(stringLength('Text string for count length', 30));
+
