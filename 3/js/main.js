bb361f8704ca34066c83a07b6eaef288163622b2
Keks <keks@htmlacademy.ru>
2022-06-09 00:05:45 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,5 +1,5 @@
 // Template names for comments
-const commenterNameList = ['Александр', 'Матвей', 'Артем', 'Михаил', 'Иван', 'Тимофей', 'Кирилл', 'Дмитрий', 'Даниил', 'Мария',
+const authorNames = ['Александр', 'Матвей', 'Артем', 'Михаил', 'Иван', 'Тимофей', 'Кирилл', 'Дмитрий', 'Даниил', 'Мария',
   'Варвара', 'Виктория', 'Алиса', 'Ксения', 'Василиса', 'Милана', 'Ева', 'Александра'];
 
 // Template text for comments
@@ -10,82 +10,65 @@ const commentTextTemplate = 'Всё отлично!\n' +
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.\n' +
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!';
 
-
 // Pick random number function
 const getRandomNumber = (from, to) => {
-  const numbersArray = [];
+  const numbers = [];
   if (from < 0 || to < 0) {
     return null;
   } else if (from > to) {
     [from, to] = [to, from];
   }
   for (let i = from; i <= to; i++) {
-    numbersArray.push(i);
+    numbers.push(i);
   }
-  return numbersArray[Math.floor(Math.random() * numbersArray.length)];
+  return numbers[Math.floor(Math.random() * numbers.length)];
 };
 
 // Check string max length function
 const checkStringLength = (string, maxlength) => string.length <= maxlength;
 
 // Get random item from array function
-const getRandomItemFromArray = (array) => array[Math.floor(Math.random() * array.length)];
+const getRandomItemFromArray = (names) => names[Math.floor(Math.random() * names.length)];
 
 // Get string from text template function
-const splitTextToStrings = (array) => (getRandomItemFromArray(array.split('\n')));
+const splitTextToStrings = (string) => (getRandomItemFromArray(string.split('\n')));
+
+const getRandomComments = (amount) => {
+  // empty array for comments generator
+  const comments = [];
+  // Generate comments
+  for (let comment = 0; comment < amount; comment++) {
+    // Generate comments object
+    const commentObject = {
+      id: comment,
+      avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+      message: splitTextToStrings(commentTextTemplate),
+      name: getRandomItemFromArray(authorNames)
+    };
+    comments.push(commentObject);
+  }
+  return comments;
+};
 
 // Elements generator function
 const generateMockObjects = (amount) => {
   // empty array for objects
-  const photosDescriptionArray = [];
-
+  const photos = [];
   // Generate basic object information
   for (let item = 1; item <= amount; item++) {
-
-    // empty array for comments generator
-    const tempCommentArray = [];
-
-    // base structure object
+    // Generate basic object
     const photoDescription = {
-      id: 0,
-      url: '',
-      description: '',
-      likes: 0,
-      comments:
-        [
-          {
-            id: 0,
-            avatar: '',
-            message: '',
-            name: ''
-          }
-        ]
+      id: item,
+      url: `photos/${item}.jpg`,
+      description: 'Some photo description',
+      likes: getRandomNumber(15, 200)
     };
-
-    photoDescription.id = item;
-    photoDescription.url = `photos/${item}.jpg`;
-    photoDescription.description = 'Some photo description';
-    photoDescription.likes = getRandomNumber(15, 200);
-
-    // Generate comments
-    for (let comment = 0; comment < getRandomNumber(1, 5); comment++) {
-      const commentObject = {
-        id: 0,
-        avatar: '',
-        message: '',
-        name: ''
-      };
-      commentObject.id = comment;
-      commentObject.avatar = `img/avatar-${getRandomNumber(1, 6)}.svg`;
-      commentObject.message = splitTextToStrings(commentTextTemplate);
-      commentObject.name = getRandomItemFromArray(commenterNameList);
-      tempCommentArray.push(commentObject);
-    }
-    photoDescription.comments = tempCommentArray;
-    photosDescriptionArray.push(photoDescription);
+    // Add comments array to basic object
+    photoDescription.comments = getRandomComments(getRandomNumber(1, 5));
+    // Push final object to array
+    photos.push(photoDescription);
   }
-
-  return photosDescriptionArray;
+  return photos;
 };
 
 generateMockObjects(25);
